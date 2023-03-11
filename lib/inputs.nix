c6b55dbbf992c162c18ec2248aeb83571d5e380b
Akira Komamura <akira.komamura@gmail.com>
2023-03-11 20:16:50 +0900

    Work around counsel

--- lib/inputs.nix
+++ lib/inputs.nix
@@ -32,6 +32,17 @@ with builtins; {
       ref = "fix-build-error";
     };
   };
+  counsel = _: super: {
+    files = removeAttrs super.files [
+      "elpa.el"
+      "ivy-avy.el"
+      "ivy-faces.el"
+      "ivy-hydra.el"
+      "ivy-overlay.el"
+      "ivy.el"
+      "swiper.el"
+    ];
+  };
   rustic = _: super: {
     packageRequires =
       {
